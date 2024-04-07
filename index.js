bc4e5ba2b475615a443f34e7e7b4f4fb8b04445a
omelette1 <stale1735@gmail.com>
2024-04-07 13:43:30 -0700

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(buildPath));
 
 
 const corsOptions={
-  origin:'http://localhost:3000',
+  origin:'https://porforliobackend.onrender.com',
   optionsSuccessStatus:200
 }
 app.use(cors(corsOptions))
